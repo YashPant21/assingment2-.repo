c9f330670299485669383e8257bd9e1de328bc34
YashPant21 <pantyash774@gmail.com>
2025-10-11 17:24:43 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+opretor_1 =int(input(' Enter operand1: '))
+opretor_2 =int(input(' Enter operand2: '))
+opretor_3 =int(input(' Enter operand3: '))
+opretor=input(' Enter the operator (+, -, *, /, %): ')
+if opretor == '+':
+    print(opretor_1 + opretor_2)
+elif opretor == '-':
+    print(opretor_1 - opretor_2)
+elif opretor == '*':
+    print(opretor_1 * opretor_2)
+elif opretor == '/':
+    print(opretor_1 / opretor_2)
+else:
+    print("none")
+
+print("calulated")
